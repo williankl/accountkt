facfe5fb2d5d7a76ff66fa8b0aae0128031ba69f
Willian Mota Oliveira <wmoliveira1000@gmail.com>
2023-12-28 16:53:48 +0100

    feat: removed non serializable issue

--- ui/application/src/commonMain/kotlin/williankl/accountkt/ui/application/ApplicationContent.kt
+++ ui/application/src/commonMain/kotlin/williankl/accountkt/ui/application/ApplicationContent.kt
@@ -3,27 +3,34 @@ package williankl.accountkt.ui.application
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.navigator.bottomSheet.BottomSheetNavigator
 import cafe.adriel.voyager.transitions.SlideTransition
+import williankl.accountkt.ui.application.currency.ConverterStateHandler.Companion.LocalConverterStateHandler
+import williankl.accountkt.ui.application.currency.ConverterStateHandler.Companion.rememberConverterStateHandler
 import williankl.accountkt.ui.application.currency.CurrencyDisplayScreen
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 public fun ApplicationContent() {
-    BottomSheetNavigator(
-        sheetBackgroundColor = Color.White,
-        scrimColor = Color.Black.copy(alpha = 0.25f),
-        sheetShape = RoundedCornerShape(
-            topStart = 16.dp,
-            topEnd = 16.dp,
-        ),
-        content = {
-            Navigator(CurrencyDisplayScreen()) {
-                SlideTransition(it)
+    CompositionLocalProvider(
+        LocalConverterStateHandler provides rememberConverterStateHandler()
+    ) {
+        BottomSheetNavigator(
+            sheetBackgroundColor = Color.White,
+            scrimColor = Color.Black.copy(alpha = 0.25f),
+            sheetShape = RoundedCornerShape(
+                topStart = 16.dp,
+                topEnd = 16.dp,
+            ),
+            content = {
+                Navigator(CurrencyDisplayScreen()) {
+                    SlideTransition(it)
+                }
             }
-        }
-    )
+        )
+    }
 }
--- ui/application/src/commonMain/kotlin/williankl/accountkt/ui/application/currency/ConverterStateHandler.kt
+++ ui/application/src/commonMain/kotlin/williankl/accountkt/ui/application/currency/ConverterStateHandler.kt
@@ -1,15 +1,39 @@
 package williankl.accountkt.ui.application.currency
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ProvidableCompositionLocal
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.staticCompositionLocalOf
+import williankl.accountkt.data.currencyService.models.Symbol
+import williankl.accountkt.feature.currencyFeature.models.CurrencyRate
 
-internal class ConverterStateHandler(
-    initSymbol: String,
+public class ConverterStateHandler(
+    initSymbol: Symbol,
     initRatio: Float,
 ) {
-    var symbol by mutableStateOf(initSymbol)
-    var ratio by mutableFloatStateOf(initRatio)
+    public companion object {
+        @Composable
+        public fun rememberConverterStateHandler(
+            initSymbol: Symbol = "BRL",
+            initRatio: Float = 1.0f,
+        ): ConverterStateHandler = remember {
+            ConverterStateHandler(initSymbol, initRatio)
+        }
+
+        public val LocalConverterStateHandler: ProvidableCompositionLocal<ConverterStateHandler?> =
+            staticCompositionLocalOf { null }
+    }
+
+    public var symbol: String by mutableStateOf(initSymbol)
+        internal set
+
+    public var ratio: Float by mutableFloatStateOf(initRatio)
+        internal set
+
+    public var supportedTargetSymbols: List<CurrencyRate> by mutableStateOf(emptyList())
+        internal set
 }
