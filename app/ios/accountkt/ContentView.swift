d145433f66a120dd3b449989a48120028324d2ff
Willian Mota Oliveira <wmoliveira1000@gmail.com>
2023-12-30 20:18:31 +0100

    chore: not ignoring safe areas anymore

--- app/ios/accountkt/ContentView.swift
+++ app/ios/accountkt/ContentView.swift
@@ -23,6 +23,6 @@ struct ComposeView: UIViewControllerRepresentable {
 
 struct ContentView: View {
     var body: some View {
-        ComposeView().ignoresSafeArea(.all)
+        ComposeView()
     }
 }
