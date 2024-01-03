5673ab4c7a18f05386022b3d530f44e2754d3a00
Willian Mota Oliveira <wmoliveira1000@gmail.com>
2024-01-03 14:44:57 +0100

    fix: opening url

--- ui/application/src/androidMain/kotlin/williankl/accountkt/ui/application/platform/PlatformSharedActions.kt
+++ ui/application/src/androidMain/kotlin/williankl/accountkt/ui/application/platform/PlatformSharedActions.kt
@@ -2,6 +2,7 @@ package williankl.accountkt.ui.application.platform
 
 import android.content.Context
 import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 import android.net.Uri
 
 
@@ -10,6 +11,7 @@ public actual class PlatformSharedActions(
 ) {
     public actual fun openUrl(url: String) {
         val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        intent.flags += FLAG_ACTIVITY_NEW_TASK
         context.startActivity(intent)
     }
 }
