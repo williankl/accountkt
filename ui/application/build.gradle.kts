97a107c4948da597bbb75fbb131d2c08a44b75f4
Willian Mota Oliveira <wmoliveira1000@gmail.com>
2024-01-08 12:00:53 +0100

    feat: added ksp and lyricist

--- ui/application/build.gradle.kts
+++ ui/application/build.gradle.kts
@@ -1,15 +1,21 @@
 import com.codingfeline.buildkonfig.compiler.FieldSpec
+import williankl.accountkt.buildSrc.helpers.applyMultiModuleKsp
 import williankl.accountkt.buildSrc.helpers.applyNativeWithBaseName
+import williankl.accountkt.buildSrc.helpers.commonMainLyricistImplementation
 import williankl.accountkt.buildSrc.helpers.retrieveVersionFromCatalogs
+import williankl.accountkt.buildSrc.helpers.setupLyricist
 
 plugins {
     id("accoutkt.multiplatform")
     id("org.jetbrains.compose")
     id("dev.icerock.mobile.multiplatform-resources")
     id("com.codingfeline.buildkonfig")
+    id("com.google.devtools.ksp")
 }
 
 applyNativeWithBaseName("KtApplication")
+applyMultiModuleKsp("williankl.accountkt.ui.application")
+setupLyricist()
 
 android {
     namespace = "williankl.accountkt.ui.application"
@@ -21,6 +27,9 @@ dependencies {
     commonMainImplementation(projects.data.currencyStorage)
     commonMainImplementation(projects.data.currencyService)
     commonMainImplementation(projects.ui.design.components)
+
+    commonMainImplementation(libs.lyricist.core)
+    commonMainLyricistImplementation(libs.lyricist.ksp)
 
     commonMainImplementation(libs.kodein.compose)
     commonMainImplementation(libs.kotlinx.serialization.core)
---
+++ ui/application/src/commonMain/kotlin/williankl/accountkt/ui/application/localization/ApplicationStrings.kt
@@ -0,0 +1,6 @@
+package williankl.accountkt.ui.application.localization
+
+
+internal data class ApplicationStrings (
+    val abc: String,
+)
